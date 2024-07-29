8b792589f778b440959308a2d4cf780b27101946
JOSE CUEVAS <levelmusick@gmail.com>
2024-07-28 19:56:48 -0700

    1 commit

---
+++ Aritmeticos .js
@@ -0,0 +1,28 @@
+// console.log("HOLA MUNDO")
+//  let a = 5;
+//  let b = 7;
+
+// // console.log(a + b, "suma")
+// // console.log(a - b, "resta")
+// // console.log(a * b, "multiplicacion")
+// // console.log(a/b, "division")
+// // console.log(a % b, ",modulo") 
+// // console.log(a ** b,"potencia") //elevado a la n//
+
+// console.log(++a)
+// console.log(a++)
+// console.log(a)
+
+// let mayor = cualEsMayor(10 , 16);
+
+// function cualEsMayor(a , b){
+// return (a > b) ? a : b
+// }
+// console.log(mayor + " es mayor" )
+
+let i = 0
+while(i < 10){
+    i++
+    
+    console.log("*")
+}
